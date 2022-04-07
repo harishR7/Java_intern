e066caf5a07b34d2cf23c3d153c49b3e6536478f
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-04-07 11:45:19 +0530

    OverLoading

---
+++ Overloading/src/com/example/demo/Application.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+import java.util.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		
+		Scanner sc=new Scanner(System.in);
+		
+		Converter con =new Converter();
+		
+		// creating a name input using scanner;
+		
+		
+		System.out.println("Enter the Name:");
+		
+	    String name=sc.next();
+		
+	    
+	   // creating a Faren input using scanner;
+	    
+		
+		System.out.println("Enter the Farenheit Temp:");
+		
+		double farenTemp=sc.nextDouble();
+		
+		
+		
+		//creating a amount and from input using scanner;
+		
+		
+		System.out.println("Enter the amount:");
+		double amount=sc.nextDouble();
+		System.out.println("Enter the From:");
+		int from=sc.nextInt();
+		
+	
+		
+		System.out.println(con.convert(amount, from));
+		
+		System.out.println(con.convert(name));
+		
+		System.out.println("Celsius Temp :"+con.convert(farenTemp));
+		
+		
+		sc.close();
+		
+		
+		
+		
+	}
+
+}
+
+
---
+++ Overloading/src/com/example/demo/Converter.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class Converter {
+	
+	public String convert(String str) {
+		return str.toUpperCase();
+		
+	}
+	// Creating a overloaded Method
+	
+	public double convert(double farenTemp) {
+		return (farenTemp-32)*5/9;
+	}
+	
+	public double convert(double amt,int frm) {
+		
+		double response=amt*100;
+		if(frm==1) {
+		response=amt*120;
+		}
+		return response;
+	}
+}
