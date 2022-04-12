732455d626c6c2cddc6b462332a3d0b8841f3c18
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-04-12 12:26:14 +0530

    collection

--- Collections/src/com/training/HashMap2.java
+++ Collections/src/com/training/HashMap2.java
@@ -1,6 +1,9 @@
 package com.training;
 
+import java.util.Collection;
+
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMap2 {
 	
@@ -16,6 +19,23 @@ public class HashMap2 {
 	    map.put(902, shyam);
 	    map.put(904,mukesh);
 	    System.out.println(map.get(900));
+	    System.out.println(map.get(902));
+	    System.out.println(map.get(904));
+	    
+	    
+	    Set<Integer> keys=map.keySet();
+	   // System.out.println(keys);
+	    for(Integer key:keys) {
+	    	System.out.println(map.get(key));
+	    }
+	    
+	    Collection<Student>list=map.values();
+	    for(Student eachStudent:list) {
+	    	System.out.println(eachStudent);
+	    }
+	    
+	    
+	    
 	}
 
 }
