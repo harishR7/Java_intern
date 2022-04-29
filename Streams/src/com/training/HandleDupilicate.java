014fadeecb8e17262ef709fcf654f959b71c0a85
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-04-29 17:28:48 +0530

    streams

--- Streams/src/com/training/HandleDupilicate.java
+++ Streams/src/com/training/HandleDupilicate.java
@@ -18,9 +18,26 @@ public class HandleDupilicate {
 				.collect(toList());
 		
 		System.out.println(list);
+////////////////////////////////////////////////////////////////////////////////////////////////		
+		
+		List<Integer>list1=Arrays.asList(1,2,3,56,8);
+		list1.stream().filter(e -> e%2==0).forEach(System.out::println);
+		for(Integer eachValue :list1) {
+			System.out.println(list1.indexOf(eachValue));
+		}
 		
 		
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		List<Book> book=Arrays.asList(new Book(1001, "Harish", "BOOK", 5000),
+				                         new Book(1002, "loki", "BOAT", 4000));
+	
+		Map<String,Double>map=book.stream().collect(toMap(Book::getBookName,Book::getPrice ));
 		
+		map.forEach((key,value) -> {System.out.println(key);
+		                            System.out.println(value);
+		});
+		                           
 	}
 
 }
