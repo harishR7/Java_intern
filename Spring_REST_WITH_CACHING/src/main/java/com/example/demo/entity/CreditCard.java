8f9ad761b0952b4575808b0dbb980500454aefc2
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-05-19 18:25:05 +0530

    spring rest cache

---
+++ Spring_REST_WITH_CACHING/src/main/java/com/example/demo/entity/CreditCard.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "harish_credit_card22")
+public class CreditCard {
+
+	
+	@Id
+	@Column(name = "card_number")
+	private long cardNumber;
+	@Column(name = "card_holder_name")
+	private String cardHolderName;
+	@Column(name = "credit_limit")
+	private double creditLimit;
+}
