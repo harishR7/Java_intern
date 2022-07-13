cc6104b356dcd18cee2e0a341794637adb242a87
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-07-13 18:21:10 +0530

    cab client 1

---
+++ cab-driver-service/src/main/java/com/example/demo/services/CabDriverService.java
@@ -0,0 +1,50 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.CabDriver;
+import com.example.demo.repository.CabDriverRepository;
+
+@Service
+public class CabDriverService {
+
+	
+	private CabDriverRepository repo;
+
+	@Autowired
+	public CabDriverService(CabDriverRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	public Optional<CabDriver> findById(int id){
+		return this.repo.findById(id);
+	}
+	
+	public List<CabDriver> findAll(){
+		return this.repo.findAll();
+	}
+	
+	public CabDriver save(CabDriver entity) {
+		return this.repo.save(entity);
+	}
+	
+	public void remove(int id) {
+		Optional<CabDriver> foundElement=repo.findById(id);
+		if(foundElement.isPresent()) {
+			this.repo.deleteById(id);
+		}
+		else {
+			throw new RuntimeException("Element with "+id+"Not Present");
+		}
+	}
+	
+	public CabDriver findByDriverName(String qryName){
+		return this.repo.findByDriverName(qryName);
+	}
+	
+}
