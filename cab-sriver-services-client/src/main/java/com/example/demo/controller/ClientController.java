7167be4cdb25d1df3d9c3348a9a13872d41db6a8
Harish.R <Harish.R@EXIND3010-73.allfon.in>
2022-07-13 18:21:40 +0530

    cab client 2

---
+++ cab-sriver-services-client/src/main/java/com/example/demo/controller/ClientController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.entity.CabDriver;
+
+@RestController
+@RequestMapping(path = "/client")
+public class ClientController {
+
+	@Autowired
+	private RestTemplate template;
+	
+	@GetMapping(path = "/drivers")
+	public String getDrivers() {
+		return this.template.getForObject("http://CAB-DRIVER-SERVICE/api/v1/drivers",String.class);
+	}
+	@GetMapping(path = "/drivers/json")
+	public CabDriver[] getDriversAsJson() {
+		return this.template.getForObject("http://CAB-DRIVER-SERVICE/api/v1/drivers",CabDriver[].class);
+	}
+}
